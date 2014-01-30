d76ce7f8d2f5edac6a6be9d6ee7aeac6cfdd72d3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-30 16:14:23 -0500

    Leaning on Hydramata configuration for theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  class_attribute :theme
 
-  self.theme = 'hydramata'
+  class_attribute :theme
+  self.theme = Hydramata.configuration.layout.theme
   helper_method :theme
 end
---
+++ config/initializers/hydramata_configuration.rb
@@ -0,0 +1,8 @@
+# Make over-rides to the configuration here.
+#
+# Want to know what all is in the configuration? Try one of the following:
+# * Hydramata.configuration.inspect (a Hash structure)
+# * Hydramata.configuration.keys (just the top-level names)
+Hydramata.configure do |config|
+  # config.layout.theme = "hydramata"
+end
