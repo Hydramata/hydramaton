ab397d1906c31283fa627475eb70b723c4804049
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-29 20:09:23 -0500

    Tidying up Core
    Things can be moved there as needed.

--- hydramata-core/lib/hydramata/core/engine.rb
+++ hydramata-core/lib/hydramata/core/engine.rb
@@ -14,14 +14,8 @@ module Hydramata::Core
     engine_name 'hydramata_core'
 
     initializer 'hydramata-core.initializers' do |app|
-      app.config.paths.add 'app/finalizers', eager_load: true
-      app.config.paths.add 'app/forms', eager_load: true
-      app.config.paths.add 'app/resolvers', eager_load: true
       app.config.paths.add 'app/services', eager_load: true
       app.config.autoload_paths += %W(
-        #{config.root}/app/finalizers
-        #{config.root}/app/forms
-        #{config.root}/app/resolvers
         #{config.root}/app/services
       )
     end
--- hydramata-core/spec/services/hydramata/core/stub_writing_service_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-module Hydramata::Core
-  describe StubWritingService do
-    context '#call' do
-      it 'should persist'
-    end
-  end
-end
