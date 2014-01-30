ff7826e884e5d75c7c0d141bd3980f4f3da51af6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-30 10:40:38 -0500

    Updating creator to

--- db/migrate/20140130153920_create_hydramata_core_work_draft.hydramata_core.rb
+++ db/migrate/20140130153920_create_hydramata_core_work_draft.hydramata_core.rb
@@ -1,10 +1,14 @@
 # This migration comes from hydramata_core (originally 20140129150843)
 class CreateHydramataCoreWorkDraft < ActiveRecord::Migration
   def change
-    create_table :hydramata_core_work_drafts do |t|
+    create_table :hydramata_core_work_drafts, primary_key: false do |t|
       t.string :pid, unique: true
       t.string :work_type, limit: 64, index: true
+      t.integer :creator
+      t.string :creator_type, limit: 64
       t.text :serialized_attributes, limit: 2147483647
     end
+
+    add_index :hydramata_core_work_drafts, [:creator, :creator_type]
   end
 end
