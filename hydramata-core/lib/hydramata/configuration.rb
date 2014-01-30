220269ba11f4a6c98449569cca5138fc4c7d9b25
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-30 16:13:47 -0500

    Adding commentary on Hydramata::Configuration

--- hydramata-core/lib/hydramata/configuration.rb
+++ hydramata-core/lib/hydramata/configuration.rb
@@ -1,5 +1,12 @@
 require 'configatron'
 module Hydramata
+  # It is my intention that the configuration has top-level keys that have a
+  # corresponding plugin. That is to say configurations for Hydramata::Deposit
+  # would have a corresponding Hydramata.configuration.deposit entry.
+  #
+  # That said, I'm not entirely certain how things will need to be configured.
+  # So if you introduce something to the configuration make sure that you are
+  # writing tests.
   class Configuration < ::Configatron::Store
   end
 end
