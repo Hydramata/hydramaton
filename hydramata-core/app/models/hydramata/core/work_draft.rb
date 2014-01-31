4627e80e5536934b0c545a315c4fcae4b8752393
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-31 14:59:07 -0500

    Adding WorkDraft.query
    We need to retrieve objects for querying

--- hydramata-core/app/models/hydramata/core/work_draft.rb
+++ hydramata-core/app/models/hydramata/core/work_draft.rb
@@ -1,6 +1,15 @@
 class Hydramata::Core::WorkDraft < ActiveRecord::Base
   serialize :attributes_store
 
+
+  def self.query(controller, parameters)
+    if work_type = parameters[:work_type]
+      where(work_type: work_type)
+    else
+      all
+    end
+  end
+
   def self.fetch_work_type_by_pid(pid)
     (where(pid: pid).pluck(:work_type).first) || raise(ActiveRecord::RecordNotFound.new("Unable to find #{self} PID:#{pid.inspect}"))
   end
--- hydramata-core/spec/models/hydramata/core/work_draft_spec.rb
+++ hydramata-core/spec/models/hydramata/core/work_draft_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 module Hydramata::Core
   describe WorkDraft do
+    let(:work_type) { 'article' }
+    let(:pid) { '1234' }
+
     context '.fetch_work_type_by_pid' do
-      let(:pid) { '1234' }
-      let(:work_type) { 'article' }
       before(:each) do
         Hydramata::Core::WorkDraft.create(pid: pid, work_type: work_type)
       end
@@ -17,6 +18,21 @@ module Hydramata::Core
         expect {
           described_class.fetch_work_type_by_pid(pid + '-not-found')
         }.to raise_error
+      end
+    end
+
+    context '.query' do
+      let(:controller) { double(:controller) }
+      let(:parameters) { { work_type: work_type} }
+      let(:other_work_type) { 'generic_work' }
+      let(:other_pid) { '1234' }
+
+      let!(:work_1) { Hydramata::Core::WorkDraft.create(pid: pid, work_type: work_type) }
+      let!(:work_2) { Hydramata::Core::WorkDraft.create(pid: other_pid, work_type: other_work_type) }
+
+      subject { described_class }
+      it 'returns the work_type if the object exists' do
+        expect(subject.query(controller, parameters)).to eq([work_1])
       end
     end
   end
