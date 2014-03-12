49b5100c753859c893794b92dd47e8534ae0e0e6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-12 12:48:02 -0400

    Adding Group.new_form_for
    Also moving the module declaration up a level; this provides some
    namespacing shortcuts

--- app/models/hydramata/group.rb
+++ app/models/hydramata/group.rb
@@ -1,42 +1,57 @@
-class Hydramata::Group < ActiveRecord::Base
-  self.table_name = 'hydramata_groups'
+module Hydramata
+  class Group < ActiveRecord::Base
+    self.table_name = 'hydramata_groups'
 
-  class Form
-    include Virtus.model
-    include ActiveModel::Validations
-
-    attribute :name, String
-    validates :name, presence: true
-
-    attribute :group, Hydramata::Group
-    validates :group, presence: true
-
-    delegate :to_param, :to_key, :id, :persisted, to: :group
-
-    def self.model_name
-      Hydramata::Group.model_name
+    def self.new_form_for(user)
+      group = Group.new
+      Group::Form.new(group: group, creator: user)
     end
 
-    def create
-      valid? ? __create : false
-    end
-    alias_method :save, :create
+    class Form
+      include Virtus.model
+      include ActiveModel::Validations
 
-    def __create
-      group.attributes = { name: name }
-      group.save
-    end
-    private :__create
+      attribute :name, String
+      validates :name, presence: true
 
-    def update(attributes = {})
-      self.attributes = attributes
-      valid? ? __update : false
-    end
+      validates :group, presence: true
+      validates :creator, presence: { unless: lambda {|form| form.persisted? } }
 
-    def __update
-      group.update(name: name)
-    end
-    private :__update
+      attr_reader :creator, :group
+      def initialize(input = {}, &block)
+        attrs = input.with_indifferent_access
+        @group = attrs.fetch(:group)
+        @creator = attrs.delete(:creator)
+        super(input, &block)
+      end
 
+      delegate :to_param, :to_key, :id, :persisted?, to: :group
+
+      def self.model_name
+        Hydramata::Group.model_name
+      end
+
+      def create
+        valid? ? __create : false
+      end
+      alias_method :save, :create
+
+      def __create
+        group.attributes = { name: name }
+        group.save
+      end
+      private :__create
+
+      def update(attributes = {})
+        self.attributes = attributes
+        valid? ? __update : false
+      end
+
+      def __update
+        group.update(name: name)
+      end
+      private :__update
+
+    end
   end
 end
--- spec/models/hydramata/group_spec.rb
+++ spec/models/hydramata/group_spec.rb
@@ -1,41 +1,51 @@
 require 'spec_helper'
 
-describe Hydramata::Group do
-  Given(:group) { Hydramata::Group.new }
+module Hydramata
+  describe Group do
+    Given(:group) { Group.new }
 
-end
-
-describe Hydramata::Group::Form do
-  Given(:group) { Hydramata::Group.new }
-  Given(:attributes) { { group: group} }
-  Given(:form) { Hydramata::Group::Form.new(attributes) }
-
-  context 'attributes' do
-    Then { expect(form.group).to eq(group) }
+    context '.new_form_for' do
+      Given(:user) { double('Context') }
+      Given(:name) { 'my name'}
+      When(:result) { Group.new_form_for(user) }
+      Then { expect(result).to be_an_instance_of(Group::Form) }
+      And { expect(result.creator).to eq(user) }
+    end
   end
 
-  context '#save' do
-    Given(:group) { double('Group', :save => true, :attributes= => true) }
-    Given(:form) { Hydramata::Group::Form.new(group: group, name: 'my name') }
-    When(:save_result) { form.save }
-    Then { expect(save_result).to eq true }
-    And { expect(group).to have_received(:attributes=).with(name: 'my name') }
-    And { expect(group).to have_received(:save) }
-  end
+  describe Group::Form do
+    Given(:group) { Group.new }
+    Given(:attributes) { { group: group} }
+    Given(:form) { Group::Form.new(attributes) }
 
-  context '#update' do
-    Given(:group) { double('Group', :update => true) }
-    Given(:form) { Hydramata::Group::Form.new(group: group) }
-    Given(:attributes) { { name: 'my name' } }
-    When(:update_result) { form.update(attributes) }
-    Then { expect(update_result).to eq true }
-    And { expect(form.attributes).to eq(attributes.merge(group: group)) }
-    And { expect(group).to have_received(:update).with(name: 'my name') }
-  end
+    context 'attributes' do
+      Then { expect(form.group).to eq(group) }
+    end
 
-  context '.model_name' do
-    Given(:form_class) { described_class }
-    Then { expect(form_class.model_name).to eq(Hydramata::Group.model_name) }
-  end
+    context '#save' do
+      Given(:group) { double('Group', persisted?: false, :save => true, :attributes= => true) }
+      Given(:creator) { double('Creator') }
+      Given(:form) { Group::Form.new(group: group, creator: creator, name: 'my name') }
+      When(:save_result) { form.save }
+      Then { expect(save_result).to eq true }
+      And { expect(group).to have_received(:attributes=).with(name: 'my name') }
+      And { expect(group).to have_received(:save) }
+    end
 
+    context '#update' do
+      Given(:group) { double('Group', persisted?: true, :update => true) }
+      Given(:form) { Group::Form.new(group: group) }
+      Given(:attributes) { { name: 'my name' } }
+      When(:update_result) { form.update(attributes) }
+      Then { expect(update_result).to eq true }
+      And { expect(form.attributes).to eq(attributes) }
+      And { expect(group).to have_received(:update).with(name: 'my name') }
+    end
+
+    context '.model_name' do
+      Given(:form_class) { described_class }
+      Then { expect(form_class.model_name).to eq(Group.model_name) }
+    end
+
+  end
 end
