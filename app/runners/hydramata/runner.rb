62cf8414bb99bdeea1c1eb18d9e4569ebd1b6ac7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-12 14:48:16 -0400

    Adding an explanation of a Runner

--- app/runners/hydramata/runner.rb
+++ app/runners/hydramata/runner.rb
@@ -1,6 +1,20 @@
 require_dependency './app/runners/hydramata/named_callbacks'
 
 module Hydramata
+  # A Runner is responsible for performing the guts of what might traditionally
+  # be a controller's action.
+  #
+  # It exists for two reasons:
+  # * Controllers are extremely over-worked (parameter negotiation,
+  #   authentication, authorization, marshalling an object, user messages via
+  #   flash, and http response determination)
+  # * The inner method content of a controller's action is subject to change
+  #   especially as other implementors look to change the behavior.
+  #
+  # So, the Runner provides a seem in the code in which you can more readily
+  # make changes to the "inner method"  of a route. In some ways, I see this as
+  # a separation of state change and response; a somewhat analogous separation
+  # to the Command/Query separation principle.
   class Runner
     attr_reader :context
 
