c9b57d923af89fff51be2438a4e39f5ac02cb429
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-12 09:09:19 -0400

    Running migrations from previous work

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118200932) do
+ActiveRecord::Schema.define(version: 20140205004739) do
 
   create_table "activity_engine_activities", force: true do |t|
     t.integer  "user_id"
@@ -128,6 +129,57 @@ ActiveRecord::Schema.define(version: 20131118200932) do
 
   add_index "help_requests", ["created_at"], name: "index_help_requests_on_created_at", using: :btree
   add_index "help_requests", ["user_id"], name: "index_help_requests_on_user_id", using: :btree
+
+  create_table "hydramata_core_attachments", id: false, force: true do |t|
+    t.string   "pid",                   limit: 32, null: false
+    t.string   "attached_to_pid",       limit: 32, null: false
+    t.string   "attached_file_name"
+    t.string   "attached_content_type"
+    t.integer  "attached_file_size"
+    t.datetime "attached_updated_at"
+  end
+
+  add_index "hydramata_core_attachments", ["attached_to_pid"], name: "index_hydramata_core_attachments_on_attached_to_pid", using: :btree
+  add_index "hydramata_core_attachments", ["pid"], name: "index_hydramata_core_attachments_on_pid", using: :btree
+
+  create_table "hydramata_core_relationships", force: true do |t|
+    t.string  "subject_id",   limit: 32
+    t.string  "subject_type", limit: 32
+    t.string  "relationship", limit: 64
+    t.string  "target_id",    limit: 32
+    t.string  "target_type",  limit: 32
+    t.integer "creator_id"
+    t.string  "creator_type", limit: 64
+  end
+
+  add_index "hydramata_core_relationships", ["creator_id", "creator_type"], name: "hc_rel_creator", using: :btree
+  add_index "hydramata_core_relationships", ["subject_id", "subject_type"], name: "hc_rel_subject", using: :btree
+  add_index "hydramata_core_relationships", ["target_id", "target_type"], name: "hc_rel_target", using: :btree
+
+  create_table "hydramata_core_work_drafts", id: false, force: true do |t|
+    t.string  "pid",                                 null: false
+    t.string  "work_type",        limit: 64,         null: false
+    t.integer "owner_id"
+    t.string  "owner_type",       limit: 64
+    t.text    "attributes_store", limit: 2147483647
+  end
+
+  add_index "hydramata_core_work_drafts", ["owner_id", "owner_type"], name: "index_hydramata_core_work_drafts_on_owner_id_and_owner_type", using: :btree
+
+  create_table "hydramata_policies", force: true do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hydramata_policy_elements", force: true do |t|
+    t.integer  "policy_id",        null: false
+    t.string   "group_identifier", null: false
+    t.string   "action_name",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "local_authorities", force: true do |t|
     t.string "name"
