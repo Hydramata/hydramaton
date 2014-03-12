1654dbcff4abaa31540a9e9d803b1cc51fb19c38
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-12 11:32:55 -0400

    Adding Hydramata::Group::Form
    There is an underlying pattern that I'm working on determining. I'm
    working hard to avoid using Rails callbacks as they are a very
    frustrating anti-pattern.

--- app/models/hydramata/group.rb
+++ app/models/hydramata/group.rb
@@ -1,3 +1,42 @@
 class Hydramata::Group < ActiveRecord::Base
   self.table_name = 'hydramata_groups'
+
+  class Form
+    include Virtus.model
+    include ActiveModel::Validations
+
+    attribute :name, String
+    validates :name, presence: true
+
+    attribute :group, Hydramata::Group
+    validates :group, presence: true
+
+    delegate :to_param, :to_key, :id, :persisted, to: :group
+
+    def self.model_name
+      Hydramata::Group.model_name
+    end
+
+    def create
+      valid? ? __create : false
+    end
+    alias_method :save, :create
+
+    def __create
+      group.attributes = { name: name }
+      group.save
+    end
+    private :__create
+
+    def update(attributes = {})
+      self.attributes = attributes
+      valid? ? __update : false
+    end
+
+    def __update
+      group.update(name: name)
+    end
+    private :__update
+
+  end
 end
--- spec/models/hydramata/group_spec.rb
+++ spec/models/hydramata/group_spec.rb
@@ -2,4 +2,40 @@ require 'spec_helper'
 
 describe Hydramata::Group do
   Given(:group) { Hydramata::Group.new }
+
+end
+
+describe Hydramata::Group::Form do
+  Given(:group) { Hydramata::Group.new }
+  Given(:attributes) { { group: group} }
+  Given(:form) { Hydramata::Group::Form.new(attributes) }
+
+  context 'attributes' do
+    Then { expect(form.group).to eq(group) }
+  end
+
+  context '#save' do
+    Given(:group) { double('Group', :save => true, :attributes= => true) }
+    Given(:form) { Hydramata::Group::Form.new(group: group, name: 'my name') }
+    When(:save_result) { form.save }
+    Then { expect(save_result).to eq true }
+    And { expect(group).to have_received(:attributes=).with(name: 'my name') }
+    And { expect(group).to have_received(:save) }
+  end
+
+  context '#update' do
+    Given(:group) { double('Group', :update => true) }
+    Given(:form) { Hydramata::Group::Form.new(group: group) }
+    Given(:attributes) { { name: 'my name' } }
+    When(:update_result) { form.update(attributes) }
+    Then { expect(update_result).to eq true }
+    And { expect(form.attributes).to eq(attributes.merge(group: group)) }
+    And { expect(group).to have_received(:update).with(name: 'my name') }
+  end
+
+  context '.model_name' do
+    Given(:form_class) { described_class }
+    Then { expect(form_class.model_name).to eq(Hydramata::Group.model_name) }
+  end
+
 end
