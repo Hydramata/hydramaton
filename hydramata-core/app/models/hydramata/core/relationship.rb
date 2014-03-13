9ac9df347670ebacb31f7a4366c85fec64eef782
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-13 14:08:19 -0400

    Fleshing out Hydramata::Core::Relationship

--- hydramata-core/app/models/hydramata/core/relationship.rb
+++ hydramata-core/app/models/hydramata/core/relationship.rb
@@ -1,2 +1,89 @@
 class Hydramata::Core::Relationship < ActiveRecord::Base
+  validates :subject_id, presence: true
+  validates :subject_type, presence: true
+  validates :predicate, presence: true
+  validates :target_id, presence: true
+  validates :target_type, presence: true
+  validates :creator_id, presence: true
+  validates :creator_type, presence: true
+
+  def self.subjects_for(collaborators = {})
+    query(collaborators).map { |obj| obj.subject }
+  end
+
+  def self.query(collaborators = {})
+    extractor = ParameterExtractor.new(collaborators)
+    where(extractor.to_hash)
+  end
+
+  # Overwriting the initialize so that we take objects which are extracted
+  # to their composite identifier (i.e. subject becomes subject_id, subject_type)
+  def initialize(attributes = {}, &block)
+    extractor = ParameterExtractor.new(attributes)
+    super(extractor.to_hash, &block)
+  end
+
+  def subject
+    subject_type.constantize.find(subject_id)
+  end
+
+  def target
+    target_type.constantize.find(target_id)
+  end
+
+  def creator
+    creator_type.constantize.find(creator_id)
+  end
+
+  class ParameterExtractor
+    def initialize(collaborators = {})
+      @subject = collaborators[:subject]
+      @target = collaborators[:target]
+      self.predicate = collaborators[:predicate]
+      @creator = collaborators[:creator]
+    end
+
+    def to_hash
+      returning_value = {}
+      returning_value[:subject_id] = subject_id if subject_id.present?
+      returning_value[:subject_type] = subject_type if subject_type.present?
+      returning_value[:predicate] = predicate if predicate.present?
+      returning_value[:target_id] = target_id if target_id.present?
+      returning_value[:target_type] = target_type if target_type.present?
+      returning_value[:creator_id] = creator_id if creator_id.present?
+      returning_value[:creator_type] = creator_type if creator_type.present?
+      returning_value
+    end
+
+    def subject_id
+      @subject.present? ? @subject.id : nil
+    end
+
+    def subject_type
+      @subject.present? ? @subject.class.to_s : nil
+    end
+
+    def target_id
+      @target.present? ? @target.id : nil
+    end
+
+    def target_type
+      @target.present? ? @target.class.to_s : nil
+    end
+
+    def creator_id
+      @creator.present? ? @creator.id : nil
+    end
+
+    def creator_type
+      @creator.present? ? @creator.class.to_s : nil
+    end
+
+    attr_reader :predicate
+    protected
+    def predicate=(value)
+      @predicate = value.to_s.underscore
+    end
+
+  end
 end
--- hydramata-core/spec/models/hydramata/core/relationship_spec.rb
+++ hydramata-core/spec/models/hydramata/core/relationship_spec.rb
@@ -2,5 +2,22 @@ require 'spec_helper'
 
 module Hydramata::Core
   describe Relationship do
+    Given(:predicate) { 'is_author_of' }
+    Given(:creator) { double('Creator', id: '123' ) }
+    Given(:subject) { double('Subject', id: '456' ) }
+    Given(:target) { double('Target', id: '789' ) }
+    context '.query' do
+      Given!(:relationship) { Relationship.create(predicate: predicate, creator: creator, subject: subject, target: target)}
+      Then { expect(Relationship.last).to eq(relationship) }
+    end
+  end
+  describe Relationship::ParameterExtractor do
+    Given(:predicate) { 'is_author_of' }
+    Given(:creator) { double('Creator', id: '123' ) }
+    Given(:subject) { double('Subject', id: '456' ) }
+    Given(:target) { double('Target', id: '789' ) }
+    Given(:normalizer) { Relationship::ParameterExtractor.new({predicate: 'is_author_of', creator: creator, subject: subject, target: target}) }
+    When(:result) { normalizer.to_hash }
+    Then { expect(result).to eq({subject_id: subject.id, subject_type: subject.class.to_s, predicate: 'is_author_of', target_id: target.id, target_type: target.class.to_s, creator_id: creator.id, creator_type: creator.class.to_s})}
   end
 end
