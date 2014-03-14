5e9564e014110e6dc68a6703ce312dad163a876e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-14 14:13:50 -0400

    Extracting GroupServices module
    To make room for other services, I'm extracting a module.

--- app/services/hydramata/services.rb
+++ app/services/hydramata/services.rb
@@ -5,56 +5,6 @@
 #
 module Hydramata
   class Services
-
-    def all_groups
-      Hydramata::Group.all
-    end
-
-    def new_group_for(creator, attributes = {})
-      group = Hydramata::Group.new_form_for(creator)
-      group.attributes = attributes if attributes.present?
-      group
-    end
-
-    def edit_group_for(user, group_id, attributes = {})
-      group = Hydramata::Group.existing_form_for(user, group_id)
-      group.attributes = attributes if attributes.present?
-      group
-    end
-
-    def save_group(group, collaborators = {})
-      creators = Array.wrap(collaborators.fetch(:creators))
-      transaction do
-        group.save &&
-        creators.all? {|creator|
-          create_relationship(
-            subject: creator,
-            predicate: 'is_creator_of',
-            target: group.group,
-            creator: creator
-          )
-        }
-      end
-    end
-
-    def find_group(group_id)
-      Hydramata::Group.find(group_id)
-    end
-
-    def create_relationship(attributes = {})
-      Hydramata::Core::Relationship.create(attributes)
-    end
-
-    private
-    def transaction
-      returning_value = true
-      ActiveRecord::Base.transaction do
-        if ! yield
-          returning_value = false
-          raise ActiveRecord::Rollback
-        end
-      end
-      returning_value
-    end
+    include ServiceContainer::GroupServices
   end
 end
---
+++ spec/services/hydramata/service_container/group_services_spec.rb
@@ -0,0 +1,72 @@
+module Hydramata::ServiceContainer
+  class GroupServiceContainer
+    include GroupServices
+  end
+
+  describe GroupServices do
+    Given(:services) { GroupServiceContainer.new }
+    Given(:user) { FactoryGirl.build_stubbed(:user) }
+    Given(:attributes) { {name: 'Name'} }
+
+    context '#all_groups' do
+      Given!(:group) { FactoryGirl.create(:hydramata_group) }
+      When(:result) { services.all_groups }
+      Then { expect(result).to eq([group]) }
+    end
+
+    context '#find_group' do
+      Given!(:group) { FactoryGirl.create(:hydramata_group) }
+      When(:result) { services.find_group(group.id) }
+      Then { expect(result).to eq(group) }
+    end
+
+    context '#new_group_for' do
+      When(:result) { services.new_group_for(user, attributes) }
+      Then { expect(result.creator).to eq(user) }
+      And { expect(result.name).to eq(attributes.fetch(:name)) }
+      And { expect(result.class.model_name).to eq(Hydramata::Group.model_name) }
+    end
+
+    context '#edit_group_for' do
+      Given!(:group) { FactoryGirl.create(:hydramata_group) }
+      Given(:attributes) { {name: 'Hello World'} }
+      When(:result) { services.edit_group_for(user, group.id, attributes) }
+      Then { expect(result.group).to eq(group) }
+      And { expect(result.name).to eq(attributes.fetch(:name)) }
+      And { expect(result.class.model_name).to eq(Hydramata::Group.model_name) }
+    end
+
+    context '#create_relationship' do
+      Given(:creator) { double('Creator', id: '123' ) }
+      Given(:subject) { double('Subject', id: '456' ) }
+      Given(:target) { double('Target', id: '789' ) }
+      Given(:predicate) { 'is_member_of' }
+      Given(:attributes) {
+        { predicate: predicate, creator: creator, subject: subject, target: target }
+      }
+      When(:result) { services.create_relationship(attributes) }
+      Then { expect(result).to be_persisted }
+      And { expect(result).to be_an_instance_of(Hydramata::Core::Relationship) }
+
+    end
+
+    context '#save_group' do
+      Given(:user) { FactoryGirl.create(:user) }
+      Given(:group) { services.new_group_for(user, attributes) }
+      context 'valid save' do
+        When(:result) { services.save_group(group, creators: user) }
+        Then { expect(result).to eq true }
+        And { expect(group).to be_persisted }
+        And { expect(group.group.creators).to eq([user]) }
+      end
+
+      context 'invalid save' do
+        Given(:attributes) { { name: nil } }
+        When(:result) { services.save_group(group, creators: user) }
+        Then { expect(result).to eq false }
+        And { expect(group.group).to_not be_persisted }
+        And { expect(group.group.creators).to eq([]) }
+      end
+    end
+  end
+end
